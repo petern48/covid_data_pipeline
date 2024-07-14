720ed9a860a7a1a6ea5dba86949b53db63f3e2c6
Peter Nguyen <petern0408@gmail.com>
2024-07-14 15:58:55 -0600

    Add aws_helpers.py for methods read_s3_to_spark() spark_write_to_rds() and spark_read_from_rds()

---
+++ aws_helpers.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from constants import RDS_ENDPOINT, RDS_PORT, RDS_USERNAME, RDS_PASSWORD, JDBC_URL
+from pyspark.conf import SparkConf
+
+
+def read_s3_to_spark(url, spark):
+    df = pd.read_csv(url)  # seemed simpler to read s3 into pandas then spark
+    df = spark.createDataFrame(df)
+    return df
+
+
+jdbc_properties = {'user': RDS_USERNAME, 'password': RDS_PASSWORD, 'driver': 'org.postgresql.Driver'}
+
+def spark_write_to_rds(spark_df, table_name):
+    spark_df.write.jdbc(url=JDBC_URL, table=table_name, properties=jdbc_properties)
+
+def spark_read_from_rds(spark, table_name):
+    df = spark.read.jdbc(url=JDBC_URL, table=table_name, properties=jdbc_properties)
+    return df
