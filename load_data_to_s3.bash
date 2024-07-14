b8cf606f99b9fa54d211f2a76781fe9a4915675b
Peter Nguyen <petern0408@gmail.com>
2024-07-14 16:02:26 -0600

    Add load_data_to_s3.bash

---
+++ load_data_to_s3.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+aws s3 cp data/covid/epidemiology.csv s3://covid-pipeline-raw-us-west-1-dev/covid
+aws s3 cp data/covid/hospitalizations.csv s3://covid-pipeline-raw-us-west-1-dev/covid
+### aws s3 cp data/covid/weather.csv s3://covid-pipeline-raw-us-west-1-dev/covid
+
+aws s3 cp data/NASDAQ-historial.csv s3://covid-pipeline-raw-us-west-1-dev/stocks/
+aws s3 cp data/SP500-historical.csv s3://covid-pipeline-raw-us-west-1-dev/stocks/
+### included aws s3 cp data/stocks s3://covid-pipeline-raw-us-west-1-dev/stocks/
