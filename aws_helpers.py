768e23164734da7b7369e9ee61096faa65c3926c
Peter Nguyen <petern0408@gmail.com>
2024-07-16 22:49:05 -0600

    Update name of function to s3_read_to_spark()

--- aws_helpers.py
+++ aws_helpers.py
@@ -2,7 +2,7 @@ import pandas as pd
 from constants import RDS_ENDPOINT, RDS_PORT, RDS_USERNAME, RDS_PASSWORD, JDBC_URL
 
 
-def read_s3_to_spark(url, spark):
+def s3_read_to_spark(url, spark):
     df = pd.read_csv(url)  # seemed simpler to read s3 into pandas then spark
     df = spark.createDataFrame(df)
     return df
