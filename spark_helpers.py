9651180b48b4f43dee348a0464753bbf3e191127
Peter Nguyen <petern0408@gmail.com>
2024-07-22 15:52:48 -0700

    Clean up spark_helpers.py

--- spark_helpers.py
+++ spark_helpers.py
@@ -2,8 +2,6 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from constants import JDBC_DRIVER, MONGO_URI
 
-
-
 def get_spark_session_and_context():
     # Creating a spark session
     conf = SparkConf()  # create the configuration
@@ -11,8 +9,6 @@ def get_spark_session_and_context():
     conf.set('spark.executor.memory', '5gb')  # 2 Gb instead of 1
     conf.set("spark.mongodb.read.connection.uri", MONGO_URI)  #"mongodb://127.0.0.1/test.coll")
     conf.set("spark.mongodb.write.connection.uri", MONGO_URI)  #"mongodb://127.0.0.1/test.coll")
-    # .config("spark.mongodb.read.connection.uri", "mongodb://127.0.0.1/test.coll") \
-    # .config("spark.mongodb.write.connection.uri", "mongodb://127.0.0.1/test.coll") \
 
     spark = SparkSession.builder.appName('pySparkSetup')\
         .config(conf=conf)\
